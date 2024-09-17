cf3c982a4d4b2333ba3129576b59744f35b065cc
ghdtjgus76 <ghdtjgus76@naver.com>
2024-09-17 20:28:56 +0900

    feat: RN 메시지 이벤트 OS 별 분기 처리 추가

--- src/react-native/ReactNativeMessageEventHandler.ts
+++ src/react-native/ReactNativeMessageEventHandler.ts
@@ -1,14 +1,23 @@
 import BaseMessageEventHandler from "../shared/core/BaseMessageEventHandler";
+import { detectEnvironment } from "../shared/utils/environment";
 
 class ReactNativeMessageEventHandler extends BaseMessageEventHandler {
+  private environment = detectEnvironment();
+
   public addMessageEventListener() {
-    document.addEventListener("message", this.handleMessageEvent);
-    window.addEventListener("message", this.handleMessageEvent);
+    if (this.environment.os === "Android") {
+      document.addEventListener("message", this.handleMessageEvent);
+    } else {
+      window.addEventListener("message", this.handleMessageEvent);
+    }
   }
 
   public removeMessageEventListener() {
-    document.removeEventListener("message", this.handleMessageEvent);
-    window.removeEventListener("message", this.handleMessageEvent);
+    if (this.environment.os === "Android") {
+      document.removeEventListener("message", this.handleMessageEvent);
+    } else {
+      window.removeEventListener("message", this.handleMessageEvent);
+    }
   }
 }
 
