5a1fedad4c36b8dcc4bff590e685f756e5118b9d
ghdtjgus76 <ghdtjgus76@naver.com>
2024-09-20 15:33:21 +0900

    test: ReactWebViewBridge 잘못된 타입으로 메시지 통신 시도 시 onMessage 이벤트 발생시키지 않는 테스트 케이스 추가

--- src/react/__tests__/ReactWebViewBridge.messageHandling.test.ts
+++ src/react/__tests__/ReactWebViewBridge.messageHandling.test.ts
@@ -88,4 +88,37 @@ describe("ReactWebViewBridge message handling", () => {
       data: messageData,
     });
   });
+
+  it("should not trigger onMessage event if type does not match", async () => {
+    const messageType = "test_type";
+    const messageData = "test_data";
+    const message = { type: messageType, data: messageData };
+
+    const onMessageMock = jest.fn();
+    bridge.onMessage(messageType, onMessageMock);
+
+    const handleMessageEventMock = jest.fn((event: MessageEvent) => {
+      const message = JSON.parse(event.data);
+
+      onMessageMock(message);
+    });
+
+    (onMessageMock as any).handleMessageEvent = handleMessageEventMock;
+
+    const postMessageMock = jest.fn(() => {
+      const mockEvent = new MessageEvent("message", {
+        data: JSON.stringify({ type: "test_type2", data: null }),
+      });
+
+      window.dispatchEvent(mockEvent);
+    });
+
+    const nativeBridge = ReactNativeWebViewBridge.getInstance();
+    const mockWebView: Partial<WebView> = { postMessage: postMessageMock };
+    const webViewRef = { current: mockWebView } as React.RefObject<WebView>;
+
+    await nativeBridge.postMessage(webViewRef, message);
+
+    expect(onMessageMock).not.toHaveBeenCalled();
+  });
 });
