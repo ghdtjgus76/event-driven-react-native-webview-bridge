b28f27dec04ed95feaca7057898e3ae5e3156749
ghdtjgus76 <ghdtjgus76@naver.com>
2024-09-21 17:43:05 +0900

    fix: react-native 빌드 안 되는 문제 해결

--- apps/react-native/metro.config.js
+++ apps/react-native/metro.config.js
@@ -1,11 +1,19 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const path = require('path');
 
 /**
  * Metro configuration
- * https://reactnative.dev/docs/metro
+ * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    // 공식문서에도 관련 내용이 있음
+    unstable_enableSymlinks: true,
+    unstable_enablePackageExports: true,
+  },
+  watchFolders: [path.join(__dirname, '..', '..')], // 이 부분이 monorepo root의 node_modules를 참조하도록 하는듯
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
