cdd15d50ff3c0a77e33c640350c001efddd6a1db
ghdtjgus76 <ghdtjgus76@naver.com>
2024-09-23 15:23:11 +0900

    fix: react-webview-bridge aos, ios 구분 eventListener 처리 추가

--- packages/react-webview-bridge/src/core/ReactMessageEventHandler.ts
+++ packages/react-webview-bridge/src/core/ReactMessageEventHandler.ts
@@ -1,12 +1,28 @@
 import BaseMessageEventHandler from "webview-bridge-core/core/BaseMessageEventHandler";
+import { detectEnvironment } from "webview-bridge-core/utils/environment";
 
 class ReactMessageEventHandler extends BaseMessageEventHandler {
+  private isAndroid: boolean;
+
+  constructor() {
+    super();
+    this.isAndroid = detectEnvironment().isAndroid;
+  }
+
   public addMessageEventListener() {
-    window.addEventListener("message", this.handleMessageEvent);
+    if (this.isAndroid) {
+      document.addEventListener("message", this.handleMessageEvent);
+    } else if (typeof window !== "undefined" && window.addEventListener) {
+      window.addEventListener("message", this.handleMessageEvent);
+    }
   }
 
   public removeMessageEventListener() {
-    window.removeEventListener("message", this.handleMessageEvent);
+    if (this.isAndroid) {
+      document.removeEventListener("message", this.handleMessageEvent);
+    } else if (typeof window !== "undefined" && window.removeEventListener) {
+      window.removeEventListener("message", this.handleMessageEvent);
+    }
   }
 }
 
