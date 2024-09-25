5e349635b255c69f8b797632f24bb8529d598e87
ghdtjgus76 <ghdtjgus76@naver.com>
2024-09-25 23:45:12 +0900

    rename: logMessagePlugin webview-bridge-core 패키지에서 react-native-webview-bridge 패키지로 이동

--- packages/react-native-webview-bridge/src/plugins/logMessagePlugin.ts
+++ packages/react-native-webview-bridge/src/plugins/logMessagePlugin.ts
@@ -1,4 +1,4 @@
-import { WebViewBridgePlugin } from "../core/Plugin";
+import { WebViewBridgePlugin } from "webview-bridge-core/core/Plugin";
 
 export const logMessagePlugin = new WebViewBridgePlugin((message: string) => {
   console.log(message);
