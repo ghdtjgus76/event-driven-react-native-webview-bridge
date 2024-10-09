8bc61354c87b4ee32c52e1546b4754568793caaf
ghdtjgus76 <ghdtjgus76@naver.com>
2024-10-09 20:51:09 +0900

    chore: react-native-webview-bridge 패키지 빌드 설정 수정

--- packages/react-native-webview-bridge/tsup.config.ts
+++ packages/react-native-webview-bridge/tsup.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   clean: true,
+  dts: true,
   target: "es2022",
-  entry: ["src/core/ReactNativeWebViewBridge.ts"],
+  entry: [
+    "src/core/ReactNativeWebViewBridge.ts",
+    "src/plugins/navigationPlugin.ts",
+    "src/plugins/versionHandlingPlugin.ts",
+  ],
   format: ["cjs", "esm"],
 });
