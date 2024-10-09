29e7f61ae926f8dfef1c411c1c377f2549691e95
ghdtjgus76 <ghdtjgus76@naver.com>
2024-10-09 19:44:52 +0900

    test: ReactNativeWebViewBridge plugin 테스트 코드 변경된 로직 반영

--- packages/react-native-webview-bridge/src/__tests__/ReactNativeWebViewBridge.plugin.test.ts
+++ packages/react-native-webview-bridge/src/__tests__/ReactNativeWebViewBridge.plugin.test.ts
@@ -1,14 +1,32 @@
-import { WebViewBridgePlugin } from "event-driven-webview-bridge-core/core/Plugin";
-import { navigationPlugin } from "../plugins/navigationPlugin";
-import { versionHandlingPlugin } from "../plugins/versionHandlingPlugin";
 import { RefObject } from "react";
 import { WebView } from "react-native-webview";
+import VersionHandlingPlugin from "../plugins/versionHandlingPlugin";
 import ReactNativeWebViewBridge from "../core/ReactNativeWebViewBridge";
+import LogMessagePlugin from "../plugins/logMessagePlugin";
+import NavigationPlugin, {
+  NavigationPluginOptions,
+} from "../plugins/navigationPlugin";
+import { NavigationProp, ParamListBase } from "@react-navigation/native";
+
+const mockedNavigation: jest.Mocked<NavigationProp<ParamListBase>> = {
+  navigate: jest.fn(),
+  goBack: jest.fn(),
+  dispatch: jest.fn(),
+  reset: jest.fn(),
+  isFocused: jest.fn(),
+  canGoBack: jest.fn().mockReturnValue(true),
+  getParent: jest.fn(),
+  setParams: jest.fn(),
+  getId: jest.fn(),
+  getState: jest.fn(),
+  setOptions: jest.fn(),
+  addListener: jest.fn(),
+  removeListener: jest.fn(),
+};
 
 describe("WebViewBridgePlugin and PluginManager", () => {
   it("should register and execute plugins correctly", () => {
-    const pluginFunction = jest.fn();
-    const logMessagePlugin = new WebViewBridgePlugin(pluginFunction);
+    const logMessagePlugin = new LogMessagePlugin();
     const plugins = { logMessagePlugin };
     const mockWebView: Partial<WebView> = {
       postMessage: jest.fn(),
@@ -18,19 +36,21 @@ describe("WebViewBridgePlugin and PluginManager", () => {
       plugins,
       webViewRef,
     });
+    const consoleLogMock = jest
+      .spyOn(console, "log")
+      .mockImplementation(() => {});
 
-    instance.triggerPluginActions("logMessagePlugin", "message");
+    instance.triggerPluginActions("logMessagePlugin", { message: "message" });
 
-    expect(pluginFunction).toHaveBeenCalled();
-    expect(pluginFunction).toHaveBeenCalledWith("message");
+    expect(consoleLogMock).toHaveBeenCalled();
+    expect(consoleLogMock).toHaveBeenCalledWith("message");
 
     ReactNativeWebViewBridge.getInstance({ webViewRef }).cleanup();
     jest.clearAllMocks();
   });
 
   it("should throw an error if a non-existent plugin is triggered", () => {
-    const pluginFunction = jest.fn();
-    const logMessagePlugin = new WebViewBridgePlugin(pluginFunction);
+    const logMessagePlugin = new LogMessagePlugin();
     const plugins = { logMessagePlugin };
     const mockWebView: Partial<WebView> = {
       postMessage: jest.fn(),
@@ -50,9 +70,7 @@ describe("WebViewBridgePlugin and PluginManager", () => {
   });
 
   it("should return the correct plugins list", () => {
-    const logMessagePlugin = new WebViewBridgePlugin((message: string) =>
-      console.log(message)
-    );
+    const logMessagePlugin = new LogMessagePlugin();
     const plugins = { logMessagePlugin };
     const mockWebView: Partial<WebView> = {
       postMessage: jest.fn(),
@@ -72,83 +90,77 @@ describe("WebViewBridgePlugin and PluginManager", () => {
 
 describe("NavigationPlugin", () => {
   it("should handle push navigation correctly", () => {
-    const navigateMock = jest.fn();
-    const navigation = { navigate: navigateMock };
-    const options = {
+    const navigationPlugin = new NavigationPlugin();
+    const options: NavigationPluginOptions = {
       action: "push",
       params: { screen: "Home" },
-      navigation,
+      navigation: mockedNavigation,
     };
 
     navigationPlugin.execute(options);
 
-    expect(navigateMock).toHaveBeenCalledWith("Home", undefined);
+    expect(mockedNavigation.navigate).toHaveBeenCalledWith("Home", undefined);
   });
 
   it("should handle pop navigation correctly", () => {
-    const goBackMock = jest.fn();
-    const canGoBackMock = jest.fn(() => true);
-    const navigation = { goBack: goBackMock, canGoBack: canGoBackMock };
-    const options = {
+    const navigationPlugin = new NavigationPlugin();
+    const options: NavigationPluginOptions = {
       action: "pop",
-      navigation,
+      navigation: mockedNavigation,
     };
 
     navigationPlugin.execute(options);
 
-    expect(canGoBackMock).toHaveBeenCalled();
-    expect(goBackMock).toHaveBeenCalled();
+    expect(mockedNavigation.canGoBack).toHaveBeenCalled();
+    expect(mockedNavigation.goBack).toHaveBeenCalled();
   });
 
   it("should handle replace navigation correctly", () => {
-    const dispatchMock = jest.fn();
-    const navigation = { dispatch: dispatchMock };
-    const options = {
+    const navigationPlugin = new NavigationPlugin();
+    const options: NavigationPluginOptions = {
       action: "replace",
       params: { screen: "Profile" },
-      navigation,
+      navigation: mockedNavigation,
     };
 
     navigationPlugin.execute(options);
 
-    expect(dispatchMock).toHaveBeenCalledWith({
+    expect(mockedNavigation.dispatch).toHaveBeenCalledWith({
       ...options.params,
       type: "REPLACE",
     });
   });
 
   it("should handle reset navigation correctly", () => {
-    const dispatchMock = jest.fn();
-    const navigation = { dispatch: dispatchMock };
-    const options = {
+    const navigationPlugin = new NavigationPlugin();
+    const options: NavigationPluginOptions = {
       action: "reset",
       params: {
         index: 0,
-        actions: [{ type: "Navigate", routeName: "Home" }],
+        actions: [{ type: "Navigate", payload: { name: "Home" } }],
       },
-      navigation,
+      navigation: mockedNavigation,
     };
 
     navigationPlugin.execute(options);
 
-    expect(dispatchMock).toHaveBeenCalledWith({
+    expect(mockedNavigation.dispatch).toHaveBeenCalledWith({
       ...options.params,
       type: "RESET",
     });
   });
 
   it("should handle setParams navigation correctly", () => {
-    const setParamsMock = jest.fn();
-    const navigation = { setParams: setParamsMock };
-    const options = {
+    const navigationPlugin = new NavigationPlugin();
+    const options: NavigationPluginOptions = {
       action: "setParams",
       params: { key: "key1", params: { user: "user1" } },
-      navigation,
+      navigation: mockedNavigation,
     };
 
     navigationPlugin.execute(options);
 
-    expect(setParamsMock).toHaveBeenCalledWith(options.params);
+    expect(mockedNavigation.setParams).toHaveBeenCalledWith(options.params);
   });
 });
 
@@ -161,22 +173,24 @@ describe("VersionHandlingPlugin", () => {
 
     const versionHandlers = {
       "0.0.0": {
-        logMessage1: (message: string) => console.log(`${message}1`),
+        logMessage1: ({ message }: { message: string }) =>
+          console.log(`${message}1`),
       },
       "1.0.0": {
-        logMessage1: (message: string) => console.log(`${message}2`),
+        logMessage1: ({ message }: { message: string }) =>
+          console.log(`${message}2`),
       },
       "2.0.0": {
-        logMessage1: (message: string) => console.log(`${message}3`),
+        logMessage1: ({ message }: { message: string }) =>
+          console.log(`${message}3`),
       },
     };
 
-    versionHandlingPlugin.execute(
-      versionHandlers,
-      currentVersion,
-      "logMessage1",
-      "message"
-    );
+    const versionHandlingPlugin = new VersionHandlingPlugin(versionHandlers);
+
+    versionHandlingPlugin.execute(currentVersion, "logMessage1", {
+      message: "message",
+    });
 
     expect(consoleLogMock).toHaveBeenCalledWith("message3");
     consoleLogMock.mockRestore();
@@ -190,21 +204,22 @@ describe("VersionHandlingPlugin", () => {
 
     const versionHandlers = {
       "1.0.0": {
-        logMessage1: (message: string) => console.log(`${message}2`),
+        logMessage1: ({ message }: { message: string }) =>
+          console.log(`${message}2`),
       },
       "2.0.0": {
-        logMessage1: (message: string) => console.log(`${message}3`),
+        logMessage1: ({ message }: { message: string }) =>
+          console.log(`${message}3`),
       },
     };
 
     const targetFunctionName = "logMessage1";
 
-    versionHandlingPlugin.execute(
-      versionHandlers,
-      currentVersion,
-      targetFunctionName,
-      "message"
-    );
+    const versionHandlingPlugin = new VersionHandlingPlugin(versionHandlers);
+
+    versionHandlingPlugin.execute(currentVersion, targetFunctionName, {
+      message: "message",
+    });
 
     expect(consoleWarnMock).toHaveBeenCalledWith(
       `No handler found for function "${targetFunctionName}" and version "${currentVersion}"`
